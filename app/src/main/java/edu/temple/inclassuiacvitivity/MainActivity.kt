e56ab37e08cec97731d24fa904504a3540d69bb6
aldottexe <aldottexe@gmail.com>
2024-09-17 12:18:57 -0400

    finish TODO 4: added OnItemSelectedListener, and meathods

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,8 +2,11 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
         val numberArray = (5..100 step 5).toList()
+        //you can also use Array(20) {index -> (index + 1) * 5}
 
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
@@ -21,7 +25,22 @@ class MainActivity : AppCompatActivity() {
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run {
+                    val size = getItemAtPosition(p2)
+
+                    displayTextView.textSize = size as Float
+
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        }
 
     }
 }
